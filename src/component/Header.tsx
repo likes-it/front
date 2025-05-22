fcb3d419e659450db4b566803f7a84699deda986
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-05-22 14:06:17 +0200

    remove ugly logo

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -47,13 +47,8 @@ export default function Header({ onAddImage }: { onAddImage: () => void }) {
     <Navbar fluid rounded>
       <NavbarBrand className="pl-4">
         <Link to="/">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="mr-3 h-6 sm:h-9"
-            alt="Logo"
-          />
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            MonApp
+            SpankMe
           </span>
         </Link>
       </NavbarBrand>
